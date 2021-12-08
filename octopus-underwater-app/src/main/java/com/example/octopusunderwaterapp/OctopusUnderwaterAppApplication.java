dad64f13d8ba2d7799c9340e6eb3ac7be265d9d1
terence-octo <86947803+terence-octo@users.noreply.github.com>
2021-12-09 09:45:04 +1030

    Add files via upload

---
+++ octopus-underwater-app/src/main/java/com/example/octopusunderwaterapp/OctopusUnderwaterAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.octopusunderwaterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OctopusUnderwaterAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OctopusUnderwaterAppApplication.class, args);
+	}
+
+}
---
+++ octopus-underwater-app/src/test/java/com/example/octopusunderwaterapp/OctopusUnderwaterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.octopusunderwaterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OctopusUnderwaterAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
